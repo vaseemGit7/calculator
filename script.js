382783a71a446c481246aa6212c938e727daa850
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2023-06-25 17:30:56 +0530

    Create basic operators

---
+++ script.js
@@ -0,0 +1,18 @@
+let a = prompt("Enter the first number");
+let b = prompt("Enter the second numnber");
+
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
